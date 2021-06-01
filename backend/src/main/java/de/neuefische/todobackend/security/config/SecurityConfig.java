8a3097d9d21d2a85748a08bd9f84f3d509b95960
Jan Robert <janmartinrobert@gmail.com>
2021-06-01 12:45:54 +0200

    Add security config

---
+++ backend/src/main/java/de/neuefische/todobackend/security/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package de.neuefische.todobackend.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Bob")
+                .password("1234")
+                .roles("User");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
