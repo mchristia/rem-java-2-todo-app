8905234c0cd2bece8363b1fc002995f74b3146a8
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-06-02 10:26:45 +0200

    Add Login endpoint

---
+++ backend/src/main/java/de/neuefische/todobackend/controller/dto/LoginData.java
@@ -0,0 +1,13 @@
+package de.neuefische.todobackend.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginData {
+    private String username;
+    private String password;
+}
