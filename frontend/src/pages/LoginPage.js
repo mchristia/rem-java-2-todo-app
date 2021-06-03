442835bf5d3ac11dcfd00f650809139d41cbed0c
christophersiem <christopher.siem@gmail.com>
2021-06-03 14:40:58 +0200

    Style LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from 'react'
 import AuthContext from '../context/AuthContext'
+import styled from 'styled-components/macro'
 
 const initialState = {
   username: '',
@@ -20,26 +21,46 @@ export default function LoginPage() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username
-        <input
-          type="text"
-          name="username"
-          onChange={handleChange}
-          value={credentials.username}
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={credentials.password}
-        />
-      </label>
-      <button>Login</button>
-    </form>
+    <Wrapper>
+      <h2>Login</h2>
+      <Form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={handleChange}
+            value={credentials.username}
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={handleChange}
+            value={credentials.password}
+          />
+        </label>
+        <button>Login</button>
+      </Form>
+    </Wrapper>
   )
 }
+
+const Form = styled.form`
+  display: grid;
+  grid-gap: 10px;
+  grid-auto-columns: 1fr 1fr 1fr;
+  justify-items: center;
+
+  button {
+    width: 150px;
+  }
+`
+
+const Wrapper = styled.div`
+  h2 {
+    text-align: center;
+  }
+`
